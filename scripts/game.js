2100331f92bf1fee72ff7525ddcf022f96eccb1b
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-04-09 12:32:16 +0000

    addvar for game

---
+++ scripts/game.js
@@ -0,0 +1,13 @@
+let game = {
+    currentGame: [],
+    playerMoves: [],
+    score: 0,
+    choices: ["button1", "button2", "button3", "button4"]
+};
+
+function newGame() {
+    game.currentGame = [];
+    game.playerMoves = [];
+    game.score = 0;
+    showScore();
+}
