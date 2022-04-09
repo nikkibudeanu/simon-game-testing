38dd313a2f7e054a1fb6442425a8203886aa4835
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-04-09 12:33:00 +0000

    add tests to check if correct meys exist

--- scripts/game.js
+++ scripts/game.js
@@ -5,9 +5,4 @@ let game = {
     choices: ["button1", "button2", "button3", "button4"]
 };
 
-function newGame() {
-    game.currentGame = [];
-    game.playerMoves = [];
-    game.score = 0;
-    showScore();
-}
+module.exports = { game, newGame, showScore };
